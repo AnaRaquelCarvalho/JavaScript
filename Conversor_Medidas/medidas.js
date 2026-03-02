58795ba04cbdd285c262212cdf4e08d3d6e36ab4
Ana R. Carvalho <anarcarvalho93@gmail.com>
2026-03-02 13:59:20 -0300

    commit

--- Conversor_Medidas/medidas.js
+++ Conversor_Medidas/medidas.js
@@ -1,4 +1,4 @@
-const medida = parseFloat(prompt("Insira uma medida em metros:"))<span class="redactor-invisible-space">​</span>
+const medida = parseFloat(prompt("Insira uma medida em metros:"));
 
 const unidade = prompt(
   "Para qual unidade de medida deseja converter?" +
@@ -6,44 +6,30 @@ const unidade = prompt(
   "\n2 - centímetros (cm)" +
   "\n3 - decímetros (dm)" +
   "\n4 - decâmetros (dam)" +
-  "\n5 - hectômetro (hm)" +
-  "\n6 - quilômetro (km)"
-)<span class="redactor-invisible-space">​</span>
+  "\n5 - hectômetros (hm)" +
+  "\n6 - quilômetros (km)"
+);
 
 switch (unidade) {
   case "1":
-    alert("Resultado: " + medida + "m = " + medida * 1000 + "mm")
+    alert("Resultado: " + medida + "m = " + (medida * 1000) + "mm");
+    break;
   case "2":
-    alert("Resultado: " + medida + "m = " + medida * 100 + "cm")
+    alert("Resultado: " + medida + "m = " + (medida * 100) + "cm");
+    break;
   case "3":
-    alert("Resultado: " + medida + "m = " + medida * 10 + "dm")
+    alert("Resultado: " + medida + "m = " + (medida * 10) + "dm");
+    break;
   case "4":
-    alert("Resultado: " + medida + "m = " + medida / 10 + "dam")
+    alert("Resultado: " + medida + "m = " + (medida / 10) + "dam");
+    break;
   case "5":
-    alert("Resultado: " + medida + "m = " + medida / 100 + "hm")
+    alert("Resultado: " + medida + "m = " + (medida / 100) + "hm");
+    break;
   case "6":
-    alert("Resultado: " + medida + "m = " + medida / 1000 + "km")
-}<span class="redactor-invisible-space">​</span>
-
- case "1":
-    alert("Resultado: " + medida + "m = " + medida * 1000 + "mm")
-    break
-  case "2":
-    alert("Resultado: " + medida + "m = " + medida * 100 + "cm")
-    break
-  case "3":
-    alert("Resultado: " + medida + "m = " + medida * 10 + "dm")
-    break
-  case "4":
-    alert("Resultado: " + medida + "m = " + medida / 10 + "dam")
-    break
-  case "5":
-    alert("Resultado: " + medida + "m = " + medida / 100 + "hm")
-    break
-  case "6":
-    alert("Resultado: " + medida + "m = " + medida / 1000 + "km")
-    break
+    alert("Resultado: " + medida + "m = " + (medida / 1000) + "km");
+    break;
   default:
-    alert("Opção inválida!")
-    break
-}<span class="redactor-invisible-space">​</span>
+    alert("Opção inválida!");
+    break;
+}
